570e84ca1d62006d77f88b8c9e49e7ab8b64ac1a
Andrew S Myers <217982952+andrew-s-myers@users.noreply.github.com>
2025-07-12 00:44:14 +0200

    vitest building block component+ tests created

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -6,12 +6,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
-    return $request->user();
+	return $request->user();
 })->middleware('auth:sanctum');
 
-// Route::post('/register', [AuthController::class, 'register']);
-// Route::post('/login', [AuthController::class, 'login']);
-// Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 // Route::middleware('auth:sanctum')->group(function () {
 //     Route::post('/foo/bar1', [FooController::class, 'bar1Foo']);

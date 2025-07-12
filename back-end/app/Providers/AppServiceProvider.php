515161bbeccdca61145036cac6674e661a1179b1
Andrew S Myers <217982952+andrew-s-myers@users.noreply.github.com>
2025-07-12 13:56:06 +0200

    reverting to phpunit - will enhance later

--- back-end/app/Providers/AppServiceProvider.php
+++ back-end/app/Providers/AppServiceProvider.php
@@ -3,22 +3,27 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\Testing\TestCommand;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
+	/**
+	 * Register any application services.
+	 */
+	public function register(): void
+	{
+		if ($this->app->runningInConsole()) {
+			$this->commands([
+				TestCommand::class,
+			]);
+		}
+	}
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        //
-    }
+	/**
+	 * Bootstrap any application services.
+	 */
+	public function boot(): void
+	{
+		// nothing needed here
+	}
 }
